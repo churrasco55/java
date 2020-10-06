dc9cb339ef8c184f0dfc1cceffd53ca4cf96d850
churrasco55 <nikitopedroza2013@gmail.com>
2020-10-06 17:09:04 -0300

    cosas

--- tipos_de_datos/src/datos.java
+++ tipos_de_datos/src/datos.java
@@ -5,12 +5,25 @@ public class datos {
         final String dato = "hola que tal bro"; //con "Final" declaramos una constante
 
         System.out.println(dato);
-        //......
+        //...... Entrada de numeros
+
         Scanner entrada = new Scanner(System.in);
         int numero;
-        System.out.print("mete un numero rey: ");
-        numero = entrada.nextInt();
+        float numero1;
+        String palabra;
+        char letra;
 
-        System.out.println("tu numero es: "+numero);
+        System.out.print("mete unas palabritas: ");
+        palabra = entrada.nextLine();
+        System.out.print("mete una letra gil: ");
+        letra = entrada.next().charAt(0);
+        System.out.print("mete un numero entero rey: ");
+        numero = entrada.nextInt();
+        System.out.print("mete otro numero flotante rey: ");
+        numero1 = entrada.nextFloat();
+
+        System.out.println("tu numeros son: "+numero+" "+numero1);
+        System.out.println("tu palabra es: "+palabra);
+        System.out.println("tu letra es: "+letra);
     }
 }
