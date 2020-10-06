26fae24cd6b1b47dbc330b288f72083c880eb1a3
churrasco55 <nikitopedroza2013@gmail.com>
2020-10-06 16:07:33 -0300

    cosas

---
+++ holamundo/src/holamundo.java
@@ -0,0 +1,12 @@
+public class holamundo {
+    public static void main(String[] args) {
+        double numero = 1.145515;
+        char letras = 'b';
+        boolean decision = true;
+
+        System.out.print("tengo estos dedos: ");
+        System.out.println(numero);
+        System.out.println(letras);
+        System.out.println("la decision es: "+decision);
+    }
+}
